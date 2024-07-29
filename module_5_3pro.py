1681bcae9b0651aebfaae09f9c9006478c676d5d
KaliEvgenio <kalievgenio@yandex.ru>
2024-07-29 21:29:13 +0700

    Add files via upload

--- module_5_3pro.py
+++ module_5_3pro.py
@@ -1,14 +1,7 @@
 class House:
-    houses_history = []
-    def __new__(cls, *args, **kwargs):
-        cls.houses_history.append(args[0])
-        print(f'{args[0]} добавлен d историю строений.')
-        return super().__new__(cls)
-    def __init__(self,*args, **kwargs):
-        self.name=args[0]
-        self.number_of_floors=args[1]
-    def __del__(self):
-        print(f'{self.name} снесён, но он останется в истории.')
+    def __init__(self,name,floor):
+        self.name=name
+        self.number_of_floors=floor
     def go_to(self,floor):
         if 1 <= floor <= self.number_of_floors:
             for i in range(floor):
@@ -20,37 +13,75 @@ class House:
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other,House):
+            return self.number_of_floors == other.number_of_floors
+        else:
+            raise TypeError
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        else:
+            raise TypeError
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        if isinstance(other,House):
+            return self.number_of_floors > other.number_of_floors
+        else:
+            raise TypeError
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        if isinstance(other,House):
+            return self.number_of_floors >= other.number_of_floors
+        else:
+            raise TypeError
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other,House):
+            return self.number_of_floors == other.number_of_floors
+        else:
+            raise TypeError
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        if isinstance(other,House):
+            return self.number_of_floors == other.number_of_floors
+        else:
+            raise TypeError
     def __add__(self, other):
-        self.number_of_floors+=other
-        return self
+        if isinstance(other,int):
+            self.number_of_floors+=other
+            return self
+        else:
+            raise TypeError('Требуется тип операнда int')
     def __radd__(self, other):
-        self.number_of_floors+=other
-        return self
+        if isinstance(other, int):
+            self.number_of_floors += other
+            return self
+        else:
+            raise TypeError('Требуется тип операнда int')
     def __iadd__(self, other):
-        self.number_of_floors+=other
-        return self
+        if isinstance(other, int):
+            self.number_of_floors += other
+            return self
+        else:
+            raise TypeError('Требуется тип операнда int')
 
 h1 = House('ЖК Эльбрус', 10)
-print(House.houses_history)
 h2 = House('ЖК Акация', 20)
-print(House.houses_history)
-h3 = House('ЖК Матрёшки', 20)
-print(House.houses_history)
 
-# Удаление объектов
-del h2
-del h3
+print(h1)
+print(h2)
 
-print(House.houses_history)
-del h1
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+h2 = 1.5 + h2
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
